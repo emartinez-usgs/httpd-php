1b402b3a790a716f6e3cb30670f7db09485f5e25
Eric Martinez <emartinez@usgs.gov>
2018-04-30 11:00:16 -0600

    Adding OWASP penetration scans to build.

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,16 +1,19 @@
 #!/usr/bin/env grooy
 
 node {
-  def FROM_IMAGE = null
-  def IMAGE_BASE = null
-  def IMAGE_NAME = null
+  def APP_NAME = 'httpd-php'
+  def IMAGE_BASE = "${GITLAB_INNERSOURCE_REGISTRY}/devops/images"
   def IMAGE_VERSION = null
-  def NODE_VERSION = null
-  def SCM_VARS = null
+  def SCM_VARS = [:]
 
-  IMAGE_BASE = "${GITLAB_INNERSOURCE_REGISTRY}/devops/images"
-  FROM_IMAGE = "${IMAGE_BASE}/usgs/centos"
-  IMAGE_NAME = "usgs/httpd-php"
+  def FAILURE = null
+  def FROM_IMAGE = "${IMAGE_BASE}/usgs/centos"
+  def IMAGE_NAME = "usgs/${APP_NAME}"
+  def LOCAL_IMAGE = "local/${APP_NAME}:latest"
+  def OWASP_CONTAINER = "${APP_NAME}-${BUILD_ID}-OWASP"
+  def OWASP_IMAGE = "${IMAGE_BASE}/owasp/zap2docker-stable"
+  def PENTEST_CONTAINER = "${APP_NAME}-PENTEST"
+
 
   try {
     stage('Initialize') {
@@ -42,23 +45,111 @@ node {
         sh """
           docker build \
             --build-arg FROM_IMAGE=${FROM_IMAGE} \
-            -t ${IMAGE_BASE}/${IMAGE_NAME}:${IMAGE_VERSION} .
-        """
-
-        // Tag for default public Docker Hub
-        sh """
-          docker tag \
-            ${IMAGE_BASE}/${IMAGE_NAME}:${IMAGE_VERSION} \
-            ${IMAGE_NAME}:${IMAGE_VERSION}
+            -t ${LOCAL_IMAGE} .
         """
       }
     }
 
     stage('Scan') {
-      echo 'TODO :: Implement security scanning.'
+      def ZAP_API_PORT = '8090'
+      def OWASP_REPORT_DIR = "${WORKSPACE}/owasp-data"
+
+
+      // Ensure report output directory exists
+      sh """
+        if [ ! -d "${OWASP_REPORT_DIR}" ]; then
+          mkdir -p ${OWASP_REPORT_DIR}
+          chmod 777 ${OWASP_REPORT_DIR}
+        fi
+      """
+
+      // Start a container to run penetration tests against
+      sh """
+        docker run --rm --name ${PENTEST_CONTAINER} \
+          -d ${IMAGE_NAME}:${IMAGE_VERSION}
+      """
+
+      // Start a container to execute OWASP PENTEST
+      sh """
+        docker run --rm -d -u zap \
+          --name=${OWASP_CONTAINER} \
+          --link=${PENTEST_CONTAINER}:application \
+          -v ${OWASP_REPORT_DIR}:/zap/reports:rw \
+          -i ${OWASP_IMAGE} \
+          zap.sh \
+          -daemon \
+          -port ${ZAP_API_PORT} \
+          -config api.disablekey=true
+      """
+
+      // Wait for OWASP container to be ready, but not for too long
+      timeout(
+        time: 20,
+        unit: 'SECONDS'
+      ) {
+        echo 'Waiting for OWASP container to finish starting up'
+        sh """
+          set +x
+          status='FAILED'
+          while [ \$status != 'SUCCESS' ]; do
+            sleep 1;
+            status=`\
+              (\
+                docker exec -i ${OWASP_CONTAINER} \
+                  curl -I localhost:${ZAP_API_PORT} \
+                  > /dev/null 2>&1 && echo 'SUCCESS'\
+              ) \
+              || \
+              echo 'FAILED'\
+            `
+          done
+        """
+      }
+
+      // Run the penetration tests
+      ansiColor('xterm') {
+        sh """
+          PENTEST_IP='application'
+          docker exec ${OWASP_CONTAINER} \
+            zap-cli -v -p ${ZAP_API_PORT} spider \
+            http://\$PENTEST_IP/
+          docker exec ${OWASP_CONTAINER} \
+            zap-cli -v -p ${ZAP_API_PORT} active-scan \
+            http://\$PENTEST_IP/
+          docker exec ${OWASP_CONTAINER} \
+            zap-cli -v -p ${ZAP_API_PORT} report \
+            -o /zap/reports/owasp-zap-report.html -f html
+          docker stop ${OWASP_CONTAINER} ${PENTEST_CONTAINER}
+        """
+      }
+
+      // Publish results
+      publishHTML (target: [
+        allowMissing: true,
+        alwaysLinkToLastBuild: true,
+        keepAll: true,
+        reportDir: OWASP_REPORT_DIR,
+        reportFiles: 'owasp-zap-report.html',
+        reportName: 'OWASP ZAP Report'
+      ])
     }
 
     stage('Publish') {
+      ansiColor('xterm') {
+        // Create tags of local image for publication
+        sh """
+          # For internal registry
+          docker tag \
+            ${LOCAL_IMAGE} \
+            ${IMAGE_BASE}/${IMAGE_NAME}:${IMAGE_VERSION}
+
+          # For docker hub
+          docker tag \
+            ${LOCAL_IMAGE} \
+            ${IMAGE_NAME}:${IMAGE_VERSION}
+        """
+      }
+
       docker.withRegistry(
         "https://${GITLAB_INNERSOURCE_REGISTRY}",
         'innersource-hazdev-cicd'
@@ -76,17 +167,37 @@ node {
     }
   } catch (err) {
     try {
-      mail([
-        to: 'gs-haz_dev_team_group@usgs.gov',
-        from: 'noreply@jenkins',
-        subject: "Jenkins Pipeline Failed: ${env.BUILD_TAG}",
-        body: "Details: ${err}"
-      ])
+      echo 'Sending email...'
+      // mail([
+      //   to: 'gs-haz_dev_team_group@usgs.gov',
+      //   from: 'noreply@jenkins',
+      //   subject: "Jenkins Pipeline Failed: ${env.BUILD_TAG}",
+      //   body: "Details: ${err}"
+      // ])
     } catch (inner) {
       echo "An error occured while sending email. '${inner}'"
     }
 
-    currentBuild.result = 'FAILURE'
-    throw err
+    FAILURE = err
+  } finally {
+    stage('Cleanup') {
+      sh """
+        set +e
+        # Cleaning up any leftover containers...
+        docker container rm --force \
+          ${OWASP_CONTAINER} \
+          ${PENTEST_CONTAINER}
+        # Cleaning up any leftover images...
+        docker image rm --force \
+          ${DEPLOY_IMAGE} \
+          ${LOCAL_IMAGE}
+        exit 0
+      """
+
+      if (FAILURE) {
+        currentBuild.result = 'FAILURE'
+        throw FAILURE
+      }
+    }
   }
 }
